61fdc55487ab5a975c111255adfb99257f480e20
Andrew Gabra <123777400+agabra23@users.noreply.github.com>
2023-03-13 16:29:34 -0400

    Implement JS password validation

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-const password = form.password.value;
-const confirmItem = form.confirm.value;
+const password = document.getElementById("password");
+const confirmItem = document.getElementById("confirm");
 const errorItem = document.getElementById("error");
 const formItem = document.getElementById("form");
 const button = document.getElementById("submit");
 
 function passwordsMatch() {
-  if (password != confirmItem) {
+  if (password.value != confirmItem.value) {
     passwordAlert();
+  } else {
+    errorItem.innerHTML = "";
+    password.classList.remove('alert');
+    confirmItem.classList.remove('alert');
   }
-  console.log("passwordsmatch");
 }
 
 function passwordAlert() {
-  console.log('alerttttt')
-    errorItem.innerHTML = "* Passwords do not match";
+  console.log("alerttttt");
+  password.classList.add("alert");
+  confirmItem.classList.add("alert");
+  errorItem.innerHTML = "* Passwords do not match";
 }
 
 button.onclick = () => {
   passwordsMatch();
   console.log("clicking");
+};
+
+password.onchange = () => {
+  passwordsMatch();
+};
+
+confirmItem.onchange = () => {
+  passwordsMatch();
 };
